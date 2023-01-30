9b8b01ff85bdf3386c27528ce93a75f501517a0e
vanegasw <vanegasw@augsburg.edu>
2023-01-30 14:24:26 -0600

    added basics

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import Link from 'next/link'
+
+// function Header({ title }) {
+//     return <h1>{title ? title : 'Default title'}</h1>;
+// }
+
+export default function HomePage() {
+
+    return (
+        <div>
+            <h1>"Develop. Preview. Ship. 🚀"</h1>
+        </div>
+    );
+}
