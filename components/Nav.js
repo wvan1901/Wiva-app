58e8817e6ee6b84479162c6e07d8f32983cdeee5
vanegasw <vanegasw@augsburg.edu>
2023-01-30 22:26:56 -0600

    Minor fixes + rm demo.yml

---
+++ components/Nav.js
@@ -0,0 +1,7 @@
+export default function Nav(){
+    return(
+        <div id="Nav">
+            <p>Home</p>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import react from "react";
 
+import Nav from "../components/Nav";
+
 export default function home(){
     return(
         <div>
+            <Nav />
             <h1>Hello!</h1>
             <p>This is some random text!</p>
         </div>
